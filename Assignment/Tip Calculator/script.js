15049acba89ba83441100c4fe2402352d8fe2974
SanjuManna <sanjumanna2201@gmail.com>
2023-02-04 23:01:49 +0530

    Tip Calculator Done

--- Assignment/Tip Calculator/script.js
+++ Assignment/Tip Calculator/script.js
@@ -1,1 +1,35 @@
+const billAmount = document.getElementById("billamt");
+const peopleAmount = document.getElementById("peopleamt");
+const calButton = document.getElementById("calculate");
+const option = document.getElementById("serviceQual");
+const show = document.getElementById("show");
+let amount = billAmount.value;
+let people = peopleAmount.value;
+let tip = option.value;
+let totalTip;
+calButton.addEventListener("click", calculateAmount);
+function calculateAmount() {
+    let amount = billAmount.value;
+    let people = peopleAmount.value;
+    let tip = option.value;
+
+    if (amount == "") {
+        alert("Please Enter Bill Amount");
+    }
+    else {
+        if (people == "") {
+            people = 1;
+            totalTip = (amount * tip).toFixed(2);
+            // totalTip.toFixed(2);
+            show.textContent = `Tip - ${totalTip}`;
+        }
+        else {
+            totalTip = ((amount * tip) / people).toFixed(2);
+            // totalTip.toFixed(2);
+            show.textContent = `Tip - ${totalTip} Each`;
+        }
+    }
+
+}
+
 
