26b4e20417adb6f89af712ed38f8e19736fdf721
SanjuManna <sanjumanna2201@gmail.com>
2023-02-04 17:02:21 +0530

    Commit

--- Assignment/sharable URL/script.js
+++ Assignment/sharable URL/script.js
@@ -1,1 +1,17 @@
 // console.log("Hello World");
+
+const inputName = document.getElementById("name");
+const inputYear = document.getElementById("year");
+const button = document.getElementById("button");
+const url = document.getElementById("url");
+
+button.addEventListener("click",(event)=>{
+    event.preventDefault();
+    
+ let textPlace = url.textContent;
+ let namePlace = inputName.value;
+ let yearPlace = inputYear.value;
+
+ url.innerText = `${textPlace}?name=${namePlace}&year=${yearPlace}`;
+
+})
