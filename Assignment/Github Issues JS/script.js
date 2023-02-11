6732ef977a1798f62562c4572b7ec5e4f3028521
SanjuManna <sanjumanna2201@gmail.com>
2023-02-11 22:00:24 +0530

    Commit

--- Assignment/Github Issues JS/script.js
+++ Assignment/Github Issues JS/script.js
@@ -1,13 +1,13 @@
 // Storing Elements
-const pageNumber = document.getElementById("pageNumber");
+const number = document.getElementById("number");
 const container = document.getElementById("container")
 const orderedList = document.getElementById("orderedList")
 const prev = document.getElementById("load_prev")
 const next = document.getElementById("load_next")
-prev.disabled = false;
-next.disabled = false;
+// prev.disabled = false;
+// next.disabled = false;
 
-let counter = 1;
+let pageNumberr = 0;
 window.addEventListener("load", fetchData)
 
 async function fetchData(pageNumberHere = 1) {
@@ -22,14 +22,14 @@ prev.addEventListener("click", () => {
     counter--;
     if (counter <= 1) {
         counter = 1;
-        prev.disabled = true;
+        // prev.disabled = true;
     }
     else {
-        prev.disabled = false;
+        // prev.disabled = false;
     }
-    next.disabled = false;
+    // next.disabled = false;
     pageNumber.textContent = `Page number ${counter}`;
-    console.log(counter);
+    // console.log(counter);
     // fetchData(counter);
 });
 
@@ -37,13 +37,40 @@ next.addEventListener("click", () => {
     counter++;
     if (counter >= 5) {
         counter = 5;
-        next.disabled = true;
+        // next.disabled = true;
     }
     else {
-        next.disabled = false;
+        // next.disabled = false;
     }
-    prev.disabled = false;
+    // prev.disabled = false;
     pageNumber.textContent = `Page number ${counter}`;
-    console.log(counter);
+    // console.log(counter);
     // fetchData(counter);
 });
+
+
+async function fetchIssues(page) {
+    const res = await fetch(`https://api.github.com/repositories/1296269/issues?page=${page}&per_page=5`);
+    const issues = await res.json();
+    return issues;
+}
+
+async function fetchPage() {
+    pageNumber++;
+    document.getElementById("number").innerHTML = pageNumber
+    const ol = document.getElementById("issueNames");
+    ol.innerHTML = "";
+    var datas = await fetchIssues(pageNumber)
+    for (var data of datas) {
+        var li = document.createElement("li");
+        li.textContent = data.title;
+        ol.append(li);
+    }
+}
+
+function prevPage() {
+    pageNumber = pageNumber - 2;
+    if (pageNumber == -1)
+        return;
+    fetchPage();
+}
--- Assignment/snake game/script.js
+++ Assignment/snake game/script.js
@@ -15,6 +15,8 @@ let changeTheDirOnce = false;
 let totalFoodAte = 0;
 let totalDistanceTravelled = 0;
 
+
+// GAME BOARD
 const createGameBoardPixels = () => {
     let a = "";
     for (let i = 1; i <= squareOfGamePixal; i++) {
@@ -25,3 +27,139 @@ const createGameBoardPixels = () => {
 };
 
 const gameBoardPixal = document.getElementsByClassName("gameBoardPixal");
+
+// FOOD
+let currentFoddPosition = 0;
+const createFood = () => {
+    gameBoardPixal[currentFoddPosition].classList.remove("food");
+    currentFoddPosition = Math.random();
+    currentFoddPosition = Math.floor(currentFoddPosition + squareOfGamePixal);
+
+    gameBoardPixal[currentFoddPosition].classList.add("food");
+}
+
+
+// SNAKE
+
+const leftDir = 37;
+const upDir = 38;
+const rightDir = 39;
+const downDir = 40;
+
+let positionArray = [];
+
+let snakeCurrentDirection = rightDir;
+
+const changeDirection = (newDirectionCode) => {
+    if (newDirectionCode == snakeCurrentDirection || changeTheDirOnce) return;
+
+    if (newDirectionCode == leftDir && snakeCurrentDirection != rightDir) {
+        snakeCurrentDirection = newDirectionCode;
+    }
+    else if (newDirectionCode == upDir && snakeCurrentDirection != downDir) {
+        snakeCurrentDirection = newDirectionCode;
+    }
+    else if (newDirectionCode == rightDir && snakeCurrentDirection != leftDir) {
+        snakeCurrentDirection = newDirectionCode;
+    }
+    else if (newDirectionCode == downDir && snakeCurrentDirection != upDir) {
+        snakeCurrentDirection = newDirectionCode;
+    }
+    changeTheDirOnce = true;
+};
+
+
+// SNAKE HEAD
+const currentSnakeHeadPosition = squareOfGamePixal / 2 - 1;
+
+let snakeLength = 100;
+
+const moveSnake = () => {
+    switch (snakeCurrentDirection) {
+        case leftDir:
+            --currentSnakeHeadPosition;
+            const isSnakeAtLastPixelLeft = currentSnakeHeadPosition % gamePixal == gamePixal - 1 || currentSnakeHeadPosition < 0;
+            if (isSnakeAtLastPixelLeft) {
+                currentSnakeHeadPosition = currentSnakeHeadPosition + gamePixal;
+            }
+            break;
+
+        case upDir:
+            currentSnakeHeadPosition = currentSnakeHeadPosition - gamePixal;
+            const isSnakeAtLastPixelUp = currentSnakeHeadPosition < 0;
+            if (isSnakeAtLastPixelUp) {
+                currentSnakeHeadPosition = currentSnakeHeadPosition + squareOfGamePixal;
+            }
+            break;
+
+        case rightDir:
+            ++currentSnakeHeadPosition;
+            const isSnakeAtLastPixelRight = currentSnakeHeadPosition % gamePixal == 0;
+            if (isSnakeAtLastPixelRight) {
+                currentSnakeHeadPosition = currentSnakeHeadPosition - gamePixal;
+            }
+            break;
+
+        case downDir:
+            currentSnakeHeadPosition = currentSnakeHeadPosition + gamePixal;
+            const isSnakeAtLastPixelDown = currentSnakeHeadPosition > squareOfGamePixal - 1;
+            if (isSnakeAtLastPixelDown) {
+                currentSnakeHeadPosition = currentSnakeHeadPosition - squareOfGamePixal;
+            }
+            break;
+
+        default:
+            break;
+
+    }
+
+
+    // KILL SNAKE IF IT BITE ITSELF
+
+    let nextSnakeHeadPixel = gameBoardPixal[currentSnakeHeadPosition];
+
+    if (nextSnakeHeadPixel.classList.contains("snakeBodyPixel")) {
+        clearInterval(moveSnakeInterval);
+        if (!alert(`Your Score is ${totalFoodAte}`)) {
+            window.location.reload();
+        }
+    }
+
+    nextSnakeHeadPixel.classList.add("snakeBodyPixel");
+
+    if (positionArray.length > totalFoodAte) {
+        let removeMe = positionArray[0];
+        positionArray.shift(1);
+        removeMe.classList.remove("snakeBodyPixel");
+    }
+
+    positionArray.push(nextSnakeHeadPixel);
+
+    changeTheDirOnce = false;
+    if (currentSnakeHeadPosition == currentFoddPosition) {
+        totalFoodAte++;
+        document.getElementById("pointsEarned").innerHTML = totalFoodAte;
+
+        snakeLength = snakeLength + 100;
+        createFood();
+    }
+};
+
+
+
+createGameBoardPixels();
+createFood();
+
+var moveSnakeInterval = setInterval(moveSnake, 100);
+
+addEventListener("keydown", (e) => changeDirection(e.keyCode));
+
+leftButton.onclick = () => changeDirection(leftDir);
+rightButton.onclick = () => changeDirection(rightDir);
+upButton.onclick = () => changeDirection(upDir);
+downButton.onclick = () => changeDirection(downDir);
+
+
+
+
+
